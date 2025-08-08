4acad570080a9a2101e37cf1b078ea6343931ba0
Dev-Yesung <yeasung67@gmail.com>
2025-08-09 01:13:20 +0900

    hello jooq

---
+++ src/main/java/ysng/hello_jooq/HelloJooqApplication.java
@@ -0,0 +1,13 @@
+package ysng.hello_jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJooqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ysng/hello_jooq/HelloJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package ysng.hello_jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
