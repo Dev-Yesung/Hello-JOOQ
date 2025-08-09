b5c36865c902c1b76c8530ece6a077e0b4bdd62f
Dev-Yesung <yeasung67@gmail.com>
2025-08-09 22:52:02 +0900

    hello jooq

--- src/test/java/ysng/hello_jooq/HelloJooqApplicationTests.java
+++ src/test/java/ysng/hello_jooq/HelloJooqApplicationTests.java
@@ -1,13 +1,24 @@
 package ysng.hello_jooq;
 
+import org.jooq.DSLContext;
+import org.jooq.generated.tables.JActor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloJooqApplicationTests {
 
+	@Autowired
+	DSLContext dslContext;
+
+	@DisplayName("")
 	@Test
-	void contextLoads() {
+	void test() {
+		dslContext.selectFrom(JActor.ACTOR)
+			.limit(10)
+			.fetch();
 	}
 
 }
